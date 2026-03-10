bf2275c2efae5efad686c09fdf1d10de2c0efa49
Daniel Nobre <55868687+Dan-Nobre@users.noreply.github.com>
2026-03-10 14:03:40 -0300

    adicionando dependencia de navegação

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ android {
 }
 
 dependencies {
+    implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.3")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.3")
     implementation(libs.androidx.core.ktx)
